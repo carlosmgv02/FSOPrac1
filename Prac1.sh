3b7ed8620e4df5fa650fac73f62e504b7ab47640
Carlos <carlos.martinezg@estudiants.urv.cat>
2022-03-09 20:08:35 +0100

    Archivo, asignacion variables y comprobar si datos correctos
    ADDED:
    -Avances en la lectura de fichero, hay que averiguar como hacer if con floats para poder tratar los valores.
    -Leemos bien el archivo, lo guardamos en las variables, y falta acabar de comprobar que los valores sean correctos, y si no se los pedimos al usuario

--- Prac1.sh
+++ Prac1.sh
@@ -14,12 +14,19 @@ temp=fil
 posFilaPal=0
 posColPal=0
 
+posFilaPil=0
+posColPil=0
+velFil=0
+velCol=0
+
+
 Nflag=''
 Fflag=''
 Cflag=''
 Pflag=''
 
 found='false'
+extraPilotes='false'
 function readFile
 {
 	#falta arreglar, sólo lee si hay 2 líneas en el archivo
@@ -38,12 +45,94 @@ function writeFile
 	echo "$fil $col $portSize"
 	
 }
+function comprobarValores
+{
+	if [ $fil -lt $minFil ] || [ $fil -gt $maxFil ] ; then
+		read -p "Files incorrectes, torna a provar: " fil
+	fi
+
+	if [ $cols -lt $minCol ] || [ $cols -gt $maxCol ] ; then
+		read -p "Columnes incorrectes, torna a provar: " cols
+	fi
+	if [ $portSize -lt $minPort ] || [ $portSize -gt $maxPort ] ; then
+		read -p "Porteria incorrecta, torna a provar: " portSize
+	fi
+	if [ $posFilaPal -lt 2 ] || [ $posFilaPal -gt 118 ] ; then
+		read -p "posFilaPal incorrecta, torna a provar: " posFilaPal
+	fi
+	if [ $posColPal -lt 2 ] || [ $posColPal -gt 35 ] ; then
+		read -p "posColPal incorrecta, torna a provar: " posColPal
+	fi
+	if [ $midaPaleta -lt 3 ] || [ $midaPaleta -gt $maxPort ] ; then
+		read -p "Mida paleta incorrecta, torna a provar: " midaPaleta
+	fi
+	if [ $posFilaPil -lt 2 ] || [ $posFilaPil -gt 118 ] ; then
+		read -p "posFilaPil incorrecta, torna a provar: " posFilaPil
+	fi
+	if [ $posColPil -lt 2 ] || [ $posColPil -gt 35 ] ; then
+		read -p "posColPil incorrecta, torna a provar: " posColPil
+	fi
+	if [ $velFil -lt -1 ] ; then
+		echo "Prueba"
+	fi
+
+	
+
+
+	
+}
 function comprobarfichero
 {
+	carlos='carlos'
+	file='/home/milax/FSOPrac1/params.txt'
 	if [ -f $file ] && [ -e $file ]
 	then
-		echo "Existe el archivo '$file'"
+		
+		num=0
+		while read line; do
+		if [ $num -eq 0 ]
+		then
+			fil=`echo $line | cut -d' ' -f1`
+			cols=`echo $line | cut -d' ' -f2`
+			portSize=`echo $line | cut -d' ' -f3`
+		elif [ $num -eq 1 ]
+		then
+			posFilaPal=`echo $line | cut -d' ' -f1`
+			posColPal=`echo $line | cut -d' ' -f2`
+			midaPaleta=`echo $line | cut -d' ' -f3`
+
+		elif [ $num -eq 2 ] 
+        then
+            posFilaPil=`echo $line | cut -d' ' -f1`
+            echo "$posFilaPil"
+            posColPil=`echo $line | cut -d' ' -f2`
+            echo "$posColPil"
+            velFil=`echo $line | cut -d' ' -f3`
+            echo "$velFil"
+            velCol=`echo $line | cut -d' ' -f4`
+            echo "$velCol"
+        
+
+		elif [ $num -eq 3 ] && [ ! -z "$line" ]
+		then
+			extraPilotes='true'
+			posFilaPil=`echo $line | cut -d' ' -f1`
+            echo "$posFilaPil"
+			
+            posColPil=`echo $line | cut -d' ' -f2`
+            echo "$posColPil"
+            velFil=`echo $line | cut -d' ' -f3`
+            echo "$velFil"
+            velCol=`echo $line | cut -d' ' -f4`
+            echo "$velCol"
+		fi
+    	let num=num+1
+
+		
 		found='true'
+		done < $file
+		let maxPort=$fil-1
+		
 	else
 		echo "No existe el archivo $file, procedemos a crearlo"
 		touch $file	#si no existe el fichero lo creamos
@@ -91,13 +180,15 @@ function readFromKeyboard
 		readFile
 		echo -n "$portSize" >> $file
 }
+comprobarfichero
+echo $fil
+comprobarValores
 echo "BIENVENIDO/A AL PROGRAMA"
 #primero de todo comprobamos que el fichero deseado existe
-comprobarfichero
 #si el fichero existe debemos comprobar que todos los parametros estan en regla
-readFile
+	#readFile
 #en caso contrario pediremos los parametros que faltan porteclado al usuario
-readFromKeyboard
+	#readFromKeyboard
 
 
 while getopts 'n:f:c:p:m:0:1:' opcio; do
--- pruebaEscribir.sh
+++ pruebaEscribir.sh
@@ -2,13 +2,42 @@
 texto="carlos 123 431 prova"
 nwords=5
 x=1
-file='/home/milax/FSOPrac1/file'
-while [ $x -lt $nwords ] 
-do
+file='/home/milax/FSOPrac1/params.txt'
+function prueba1
+{
+    while [ $x -lt $nwords ] 
+    do
+        y=`echo $texto | cut -d' ' -f$x`
+        echo "Hola: $y"
+        echo $texto | cut -d' ' -f$x
+        let x=x+1
+        echo -n "$texto " > $file
+        #echo "$1, $2"
+    done
+}
+
+function prueba2
+{
+    num=0
+    while read line; do
+    echo "Line= $line"
+    z=`echo $line | cut -d' ' -f$x`
     
-    echo $texto | cut -d' ' -f$x
+    echo "Separated: $z"
     let x=x+1
-    echo -n "$texto " > $file
-    //echo "$1, $2"
-done
+        if [ $num -eq 2 ] 
+        then
+            posFilaPil=`echo $line | cut -d' ' -f1`
+            echo $posFilaPil
+            posColPil=`echo $line | cut -d' ' -f2`
+            echo $posColPil
+            velFil=`echo $line | cut -d' ' -f3`
+            echo $velFil
+            velCol=`echo $line | cut -d' ' -f4`
+            echo $velCol
+        fi
+    let num=num+1
+    done < $file
+}
+prueba2
 #echo $var | cut -d' ' -f1
